1675340de61505cbf4627b129f3a36294b119061
Noah <noahshomette@gmail.com>
2022-10-17 01:37:48 -0400

    import needed libraries. Attempt to setup wasm-server-runner
    Signed-off-by: Noah <noahshomette@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-mod player;
 mod game_systems;
+mod player;
 
-use crate::player::*;
 use crate::game_systems::*;
+use crate::player::*;
 use bevy::prelude::*;
+use bevy::render::texture::ImageSettings;
 use bevy::window::{close_on_esc, WindowMode};
 
 fn main() {
     App::new()
+        //default resources needed
         .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(ImageSettings::default_nearest())
         .insert_resource(WindowDescriptor {
             title: "Space Commander".to_string(),
             resizable: false,
@@ -20,13 +23,13 @@ fn main() {
         })
         // base plugins
         .add_plugins(DefaultPlugins)
-
+        //
         //game base plugins
-        .add_plugin(PlayerPlugin)
         .add_plugin(GameSystems)
-
+        .add_plugin(PlayerPlugin)
+        //
         //temp testing plugins
         .add_system(close_on_esc)
-
+        //
         .run();
 }
--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,8 +1,63 @@
 ﻿use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 pub struct PlayerPlugin;
 
+const PLAYER_PLANET_ASSET_PATH: &str = "PlanetOne.png";
+
 impl Plugin for PlayerPlugin {
-    fn build(&self, _app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup_player);
+    }
 }
 
+fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(PlayerBundle::new(asset_server));
+}
+
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Bundle)]
+pub struct PlayerBundle {
+    #[bundle]
+    pub(crate) sprite_bundle: SpriteBundle,
+    rigidbody: RigidBody,
+    collider: Collider,
+    player: Player,
+    gravity_scale: GravityScale,
+    active_events: ActiveEvents,
+    colliding_entities: CollidingEntities,
+}
+
+impl PlayerBundle {
+    pub(crate) fn new(asset_server: Res<AssetServer>) -> PlayerBundle {
+        PlayerBundle {
+            sprite_bundle: SpriteBundle {
+                sprite: Default::default(),
+                transform: Transform {
+                    translation: Vec3 {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 50.0,
+                    },
+                    rotation: Default::default(),
+                    scale: Vec3 {
+                        x: 3.0,
+                        y: 3.0,
+                        z: 1.0,
+                    },
+                },
+                global_transform: Default::default(),
+                texture: asset_server.load(PLAYER_PLANET_ASSET_PATH),
+                ..default()
+            },
+            rigidbody: RigidBody::Fixed,
+            collider: Collider::ball(0.5),
+            player: Player,
+            gravity_scale: GravityScale(0.),
+            active_events: ActiveEvents::COLLISION_EVENTS,
+            colliding_entities: Default::default(),
+        }
+    }
+}
